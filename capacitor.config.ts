56191ce7aedced78bfc2edf06c83daac49ad4722
Elbis Vallejos <elbis02miguel06@gmail.com>
2026-03-29 18:43:39 -0500

    Cambios de configuración

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.notasME',
+  appName: 'NotasME',
+  webDir: 'dist'
+};
+
+export default config;
